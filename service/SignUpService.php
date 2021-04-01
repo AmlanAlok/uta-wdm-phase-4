ab6b4eea5ebadd9f9c1430568445e9492b5f3da1
Amlan Alok <amlanalok@Amlans-MacBook-Pro.local>
2021-04-01 10:32:06 -0500

    storing data to addresses and responsible contacts completed

--- service/SignUpService.php
+++ service/SignUpService.php
@@ -34,39 +34,61 @@ class SignUpService {
 
 		$rolesRoleId = $role_id;
 
-
-
-		// $user = new User(NULL, $firstName, $lastName, $emailId, $password, $areaCode, $phoneNumber, $joiningDatetime, $rolesRoleId);
-
 		$userId = $signUpService->storeUserInformation($firstName, $lastName, $emailId, $password, $areaCode, $phoneNumber, $joiningDatetime, $rolesRoleId);
 		echo "USER-ID";
 		var_dump($userId);
 
-		$street_address = $_POST['address1'];
-		$street_address_line_2 = $_POST['address2']; 
+		$streetAddress = $_POST['address1'];
+		$streetAddressLine2 = $_POST['address2']; 
 		$city = $_POST['city']; 
 		$state = $_POST['state']; 
-		$zip_code = $_POST['zip'];
+		$zipCode = $_POST['zip'];
 		$country = $_POST['country']; 
-		$users_user_id = $userId;
+		$usersUserId = $userId;
 
-		$address = new Address(NULL, $street_address, $street_address_line_2, $city, $state, $zip_code, $country, $users_user_id);
+		// $address = new Address(NULL, $street_address, $street_address_line_2, $city, $state, $zip_code, $country, $users_user_id);
 		// var_dump($address);
+		$userAddressStoredStatus = $signUpService->storeUserAddressInformation($streetAddress, $streetAddressLine2, $city, $state, $zipCode, $country, $usersUserId);
 
 		// $responsible_contact_id = $_POST[''];
 		$name = $_POST['rname']; 
 		$address = $_POST['raddress']; 
 		$city = $_POST['rcity']; 
-		$zip_code = $_POST['rzip']; 
+		$zipCode = $_POST['rzip']; 
 		$country = $_POST['rcountry'];
-		$phone_number = $_POST['rphno']; 
-		$users_user_id = $userId;
+		$phoneNumber = $_POST['rphno']; 
+		$usersUserId = $userId;
 
-		$responsibleContact = new ResponsibleContact(NULL, $name, $address, $city, $zip_code, $country, $phone_number, $users_user_id);
+		// $responsibleContact = new ResponsibleContact(NULL, $name, $address, $city, $zip_code, $country, $phone_number, $users_user_id);
 		// var_dump($responsibleContact);
+		$responsibleContactStoredStatus = $signUpService->storeResponsibleContactInformation($name, $address, $city, $zipCode, $country, $phoneNumber, $usersUserId);
 
 
 
+	}
+
+	function storeResponsibleContactInformation($name, $address, $city, $zipCode, $country, $phoneNumber, $usersUserId){
+
+		$dbObject = new Database();
+		$dbConnection = $dbObject->getDatabaseConnection();
+
+		$sql = "INSERT into `responsible_contacts` (`responsible_contact_id`,`name`, `address`, `city`, `zip_code`, `country`, `phone_number`, `users_user_id`) values (NULL, :name, :address, :city, :zipCode, :country, :phoneNumber, :usersUserId)";
+
+		$stmt = $dbConnection->prepare($sql);
+
+		$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+		$stmt->bindValue(':address', $address, PDO::PARAM_STR);
+		$stmt->bindValue(':city', $city, PDO::PARAM_STR);
+		$stmt->bindValue(':zipCode', $zipCode, PDO::PARAM_STR);
+		$stmt->bindValue(':country', $country, PDO::PARAM_STR);
+		$stmt->bindValue(':phoneNumber', $phoneNumber, PDO::PARAM_STR);
+		$stmt->bindValue(':usersUserId', $usersUserId, PDO::PARAM_INT);
+
+		if ($stmt->execute()){
+			return 'Success';
+		} else{
+			return 'Failed';
+		}
 	}
 
 	function storeUserInformation($firstName, $lastName, $emailId, $password, $areaCode, $phoneNumber, $joiningDatetime, $rolesRoleId){
@@ -85,7 +107,7 @@ class SignUpService {
 		$stmt->bindValue(':areaCode', $areaCode, PDO::PARAM_STR);
 		$stmt->bindValue(':phoneNumber', $phoneNumber, PDO::PARAM_STR);
 		$stmt->bindValue(':joiningDatetime', $joiningDatetime, PDO::PARAM_STR);
-		$stmt->bindValue(':rolesRoleId', $rolesRoleId, PDO::PARAM_STR);
+		$stmt->bindValue(':rolesRoleId', $rolesRoleId, PDO::PARAM_INT);
 
 		if ($stmt->execute()){
 			$newUserId = $dbConnection->lastInsertId();
@@ -96,8 +118,29 @@ class SignUpService {
 
 	}
 
-	function storeUserAddressInformation(){
-		
+	function storeUserAddressInformation($streetAddress, $streetAddressLine2, $city, $state, $zipCode, $country, $usersUserId){
+
+		$dbObject = new Database();
+		$dbConnection = $dbObject->getDatabaseConnection();
+
+		$sql = "INSERT into `addresses` (`address_id`,`street_address`, `street_address_line_2`, `city`, `state`, `zip_code`, `country`, `users_user_id`) values (NULL, :streetAddress, :streetAddressLine2, :city, :state, :zipCode, :country, :usersUserId)";
+
+		$stmt = $dbConnection->prepare($sql);
+
+		$stmt->bindValue(':streetAddress', $streetAddress, PDO::PARAM_STR);
+		$stmt->bindValue(':streetAddressLine2', $streetAddressLine2, PDO::PARAM_STR);
+		$stmt->bindValue(':city', $city, PDO::PARAM_STR);
+		$stmt->bindValue(':state', $state, PDO::PARAM_STR);
+		$stmt->bindValue(':zipCode', $zipCode, PDO::PARAM_STR);
+		$stmt->bindValue(':country', $country, PDO::PARAM_STR);
+		$stmt->bindValue(':usersUserId', $usersUserId, PDO::PARAM_INT);
+
+		if ($stmt->execute()){
+			return 'Success';
+		} else{
+			return 'Failed';
+		}
+
 	}
 
 	function fetchAllRoles(){
