d7cab6e2348623b346dfd02190804607a35211fb
Amlan Alok <amlanalok@Amlans-MacBook-Pro.local>
2021-04-02 02:48:38 -0500

    This code has been deployed and tested

--- service/LoginService.php
+++ service/LoginService.php
@@ -15,7 +15,7 @@ class LoginService {
 	    $userService = new UserService();
 	    $user = $userService->getUserByEmailIdAndPassword($dbConnection, $emailId, $password);
 		$_SESSION['CURRENT_USER'] = $user;
-	    var_dump($user);
+	    // var_dump($user);
 
 	    $user_id = $user->user_id;
 	    $role_id = $user->roles_role_id;
@@ -29,33 +29,34 @@ class LoginService {
 	    	$roleService = new RoleService();
 	        $roleRecord = $roleService->getRoleNameByRoleId($dbConnection, $role_id);
 
-	        var_dump($roleRecord);
+	        // var_dump($roleRecord);
 
 	        $role_name = $roleRecord->role_name;
 	    }
 
-
 	    if ($role_name == "admin"){
-	        echo "You are ADMIN";
-	        header("Location: ../YouareonDefault/admin/admin.php?user_id=$user_id");
-	        exit;
-	    } elseif ($role_name == "subdivision manager"){
-	        echo "You are SUBDIVISION MANAGER";
-	        header("Location: ../YouareonDefault/subdivision-manager/subdivision-manager.php?user_id=$user_id");
-	        exit;
-	    } elseif (strtolower($role_name) == "building manager"){
-			
-	        echo "You are BUILDING MANAGER";
-	        header("Location: ../YouareonDefault/building-manager/building-manager.php?user_id=$user_id");
-	        exit;
-	    } elseif ($role_name == "apartment owner"){
-	        echo "You are APARTMENT OWNER";
-	        header("Location: ../YouareonDefault/apartment-owner/apartment-owner.php?user_id=$user_id");
-	        exit;
-	    } else {
-	        echo "Role does not match any preset ones. Error";
-	        exit;
-	    }
+            // echo "You are ADMIN";
+            header("Location: /YouareonDefault/admin/admin.php?user_id=$user_id");
+            // echo "     testing";
+            exit;
+        } elseif ($role_name == "subdivision manager"){
+            // echo "You are SUBDIVISION MANAGER";
+            header("Location: /YouareonDefault/subdivision-manager/subdivision-manager.php?user_id=$user_id");
+            exit;
+        } elseif (strtolower($role_name) == "building manager"){
+            
+            // echo "You are BUILDING MANAGER";
+            header("Location: /YouareonDefault/building-manager/building-manager.php?user_id=$user_id");
+            exit;
+        } elseif ($role_name == "apartment owner"){
+            // echo "You are APARTMENT OWNER";
+            header("Location: /YouareonDefault/apartment-owner/apartment-owner.php?user_id=$user_id");
+            exit;
+        } else {
+            echo "Role does not match any preset ones. Error";
+            
+        }
+
 	}
 
 	// function getUserByEmailIdAndPassword($dbConnection, $emailId, $password){
